46e529f660b83250ec7389c0a3f325148a676170
kaitlynhova <kaitlynhova@gmail.com>
2014-07-15 16:54:20 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hovabook::Application.config.secret_token = '5416661c478e50bbf6aeac43b8a96fba9ae31516bcf717f5b4c73f7f1f7b4d4e5e9f1efa1750f7b4ad2483360d8803911493974bbea3a9be672aa637ebd4fd94'

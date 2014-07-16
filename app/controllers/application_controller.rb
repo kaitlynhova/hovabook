c118b9deba6b059317be19d7e6050688c8c02850
kaitlynhova <kaitlynhova@gmail.com>
2014-07-16 18:18:16 -0500

    current user check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
+      return true
     end
   end
   

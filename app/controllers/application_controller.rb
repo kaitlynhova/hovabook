ac5656635c2fb51bbfbc55c8ca5243e211ead17e
kaitlynhova <kaitlynhova@gmail.com>
2014-07-16 19:06:32 -0500

    current_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,9 @@ class ApplicationController < ActionController::Base
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
-      return true
     end
   end
+  
+  helper_method :current_user
   
 end

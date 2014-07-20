35cd4165664aa0fa473b7e6832fc00495c940945
kaitlynhova <kaitlynhova@gmail.com>
2014-07-20 16:56:02 -0700

    gig grid style

--- app/assets/javascripts/hover.js
+++ app/assets/javascripts/hover.js
@@ -2,11 +2,15 @@
 $(document).ready(function(){
   $(".workinthathover").mouseenter(function(){
     $(this).children(".display_name").removeClass( "display_name_hidden" ).addClass( "display_name_visibile" );
+
+    $(this).children(".imgWrap").toggleClass("graybg");
     $(this).children("a").children(".index_image").css( "opacity", ".2" );
   }); 
   
   $(".workinthathover").mouseleave(function(){
     $(this).children(".display_name").removeClass( "display_name_visibile" ).addClass( "display_name_hidden" );
+
+    $(this).children(".imgWrap").toggleClass("graybg");
     $(this).children("a").children(".index_image").css( "opacity", "1" );
   }); 
 });
